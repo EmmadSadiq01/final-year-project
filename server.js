09143b0bdc84e17f33254a5c5a53bb1d90229667
emmad sadiq <emmadsadiq55@gmail.com>
2023-01-26 10:36:40 +0500

    changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use('/api/booking', require('./routes/api/booking'));
 // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, './client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build/index.html'));
+        res.sendFile(path.join(__dirname, 'client/build/index.html'));
     })
 // }
 
